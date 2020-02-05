552aa92882c0a19fd4eac4f9e3e6fa52d82271ab
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-05 11:24:19 -0500

    connected search term to album name and added if statement for type - both need to be fixed

--- script.js
+++ script.js
@@ -1,192 +1,193 @@
 var APIKey = "AIzaSyAU1fAk-8WcXwrIryta0wlz5iYnRE_c2ZA";
 
 
-// WHEN PULLING SEARCH TERM....ADD .TRIM() TO REMOVE SPACES AT BEG AND END
-
-
-
-// WHEN USER DOES A VIDEO KEYWORD SEARCH..........
 
 $("#search-btn").on("click", function () {
     $("#yt-content").empty();
-    var searchTerm = $("#album_name").val();
+    var searchTerm = $("#album_name").val().trim();
     searchTerm = searchTerm.replace(/ /g, "+");
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=" + searchTerm + "&type=video&key=" + APIKey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // FIGURE OUT LINK FOR CHANNELS AND PLAYLISTS
 
-
+    // ADD IF STATEMENT HERE
+    if ($("select[id=searchType] option:selected").val() == "videos") {
         // WHEN USER DOES A VIDEO KEYWORD SEARCH..........
-        .then(function (response) {
 
-            for (var i = 0; i < 5; i++) {
+        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=" + searchTerm + "&type=video&key=" + APIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
 
-                var videoId = response.items[i].id.videoId;
+            .then(function (response) {
 
-                var queryURL2 = "https://www.googleapis.com/youtube/v3/videos?key=" + APIKey + "&part=snippet&id=" + videoId;
-                console.log(videoId);
-                $.ajax({
-                    url: queryURL2,
-                    method: "GET"
-                })
+                for (var i = 0; i < 5; i++) {
 
-                    .then(function (response) {
+                    var videoId = response.items[i].id.videoId;
 
-                        // console.log(response)
-                        var newDiv = $("<div>");
-                        var titleDiv = $("<h5>");
-                        var linkDiv = $("<a>");
-                        var videoLink = "https://www.youtube.com/watch?v=" + videoId;
-                        var imgDiv = $("<img>");
-                        // console.log(response)
-                        // console.log(response.items[0].id)
-                        var imgSrc = response.items[0].snippet.thumbnails.medium.url;
+                    var queryURL2 = "https://www.googleapis.com/youtube/v3/videos?key=" + APIKey + "&part=snippet&id=" + videoId;
+                    console.log(videoId);
+                    $.ajax({
+                        url: queryURL2,
+                        method: "GET"
+                    })
 
-                        titleDiv.text(response.items[0].snippet.title);
-                        newDiv.append(titleDiv);
+                        .then(function (response) {
 
+                            // console.log(response)
+                            var newDiv = $("<div>");
+                            var titleDiv = $("<h5>");
+                            var linkDiv = $("<a>");
+                            var videoLink = "https://www.youtube.com/watch?v=" + videoId;
+                            var imgDiv = $("<img>");
+                            // console.log(response)
+                            // console.log(response.items[0].id)
+                            var imgSrc = response.items[0].snippet.thumbnails.medium.url;
 
-
-                        linkDiv.attr("href", videoLink);
-                        linkDiv.attr("target", "_blank");
-                        newDiv.append(linkDiv);
-
-                        imgDiv.attr("src", imgSrc);
-                        linkDiv.append(imgDiv);
-
-                        $("#yt-content").append(newDiv);
-
-                        // console.log(videoLink);
-
-                    });
-
-            }
-
-        });
+                            titleDiv.text(response.items[0].snippet.title);
+                            newDiv.append(titleDiv);
 
 
 
+                            linkDiv.attr("href", videoLink);
+                            linkDiv.attr("target", "_blank");
+                            newDiv.append(linkDiv);
+
+                            imgDiv.attr("src", imgSrc);
+                            linkDiv.append(imgDiv);
+
+                            $("#yt-content").append(newDiv);
+
+                            // console.log(videoLink);
+
+                        });
+
+                }
+
+            });
+
+    }
 
 
 
+    else if ($("select[id=searchType] option:selected").val() == "channels") {
 
 
-    // WHEN A USER SEARCHES FOR CHANNEL
-
-
-
-    // var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=twiddle&type=channel&key=" + APIKey;
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // })
-    //     // FIGURE OUT LINKS FOR CHANNELS AND PLAYLISTS
-
-
-    //     .then(function (response) {
-
-    //         for (var i = 0; i < 5; i++) {
-    //             // console.log(response);
-    //             var channelId = response.items[i].id.channelId;
-    //             console.log(channelId);
-    //             var queryURL2 = "https://www.googleapis.com/youtube/v3/channels?key=" + APIKey + "&part=snippet&id=" + channelId;
-
-    //             $.ajax({
-    //                 url: queryURL2,
-    //                 method: "GET"
-    //             })
-
-    //                 .then(function (response) {
-    //                     // console.log(response);
-    //                     var newDiv = $("<div>");
-    //                     var titleDiv = $("<h5>");
-    //                     var descriptionDiv = $("<p>");
-    //                     var linkDiv = $("<a>");
-    //                     var channelLink = "https://www.youtube.com/channel/" + channelId;
-    //                     var imgDiv = $("<img>");
-    //                     var imgSrc = response.items[0].snippet.thumbnails.medium.url;
-
-    //                     titleDiv.text(response.items[0].snippet.title);
-    //                     newDiv.append(titleDiv);
-    //                     descriptionDiv.append(response.items[0].snippet.description);
-    //                     newDiv.append(descriptionDiv);
-
-    //                     // console.log(channelId);
-
-    //                     linkDiv.attr("href", channelLink)
-    //                     linkDiv.attr("target", "_blank")
-    //                     newDiv.append(linkDiv);
-
-    //                     imgDiv.attr("src", imgSrc);
-    //                     linkDiv.append(imgDiv);
-
-    //                     $(".videoList").append(newDiv);
+        // WHEN A USER SEARCHES FOR CHANNEL
 
 
 
-    //                 });
-
-    //         }
-
-    //     });
-
-
-
-
-    // WHEN A USER SEARCHES FOR A PLAYLIST
-    // var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=phish&type=playlist&key=" + APIKey;
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // })
-
-    //     .then(function (response) {
-    //         // console.log(response);
-    //         for (var i = 0; i < 5; i++) {
-    //             var firstVideo = "";
-    //             var playlistId = response.items[i].id.playlistId;
-    //             // console.log(playlistId)
-    //             var queryURL2 = "https://www.googleapis.com/youtube/v3/playlists?key=" + APIKey + "&part=snippet&id=" + playlistId;
-
-    //             $.ajax({
-    //                 url: queryURL2,
-    //                 method: "GET"
-    //             })
-
-    //                 .then(function (response) {
-
-    //                     console.log(response)
-    //                     var newDiv = $("<div>");
-    //                     var titleDiv = $("<h5>");
-    //                     var linkDiv = $("<a>");
-    //                     var videoLink = "https://www.youtube.com/watch?v=" + playlistId;
-    //                     var imgDiv = $("<img>");
-    //                     // console.log(response)
-    //                     // console.log(response)
-    //                     var imgSrc = response.items[0].snippet.thumbnails.medium.url;
-
-    //                     titleDiv.text(response.items[0].snippet.title);
-    //                     newDiv.append(titleDiv);
+        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=twiddle&type=channel&key=" + APIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
 
 
 
-    //                     linkDiv.attr("href", videoLink);
-    //                     linkDiv.attr("target", "_blank");
-    //                     newDiv.append(linkDiv);
+            .then(function (response) {
 
-    //                     imgDiv.attr("src", imgSrc);
-    //                     linkDiv.append(imgDiv);
+                for (var i = 0; i < 5; i++) {
+                    // console.log(response);
+                    var channelId = response.items[i].id.channelId;
+                    console.log(channelId);
+                    var queryURL2 = "https://www.googleapis.com/youtube/v3/channels?key=" + APIKey + "&part=snippet&id=" + channelId;
 
-    //                     $("#yt-content").append(newDiv);
+                    $.ajax({
+                        url: queryURL2,
+                        method: "GET"
+                    })
 
-    //                     // console.log(videoLink);
+                        .then(function (response) {
+                            // console.log(response);
+                            var newDiv = $("<div>");
+                            var titleDiv = $("<h5>");
+                            var descriptionDiv = $("<p>");
+                            var linkDiv = $("<a>");
+                            var channelLink = "https://www.youtube.com/channel/" + channelId;
+                            var imgDiv = $("<img>");
+                            var imgSrc = response.items[0].snippet.thumbnails.medium.url;
 
-    //                 });
+                            titleDiv.text(response.items[0].snippet.title);
+                            newDiv.append(titleDiv);
+                            descriptionDiv.append(response.items[0].snippet.description);
+                            newDiv.append(descriptionDiv);
 
-    //         }
+                            // console.log(channelId);
 
-    //     });
+                            linkDiv.attr("href", channelLink)
+                            linkDiv.attr("target", "_blank")
+                            newDiv.append(linkDiv);
+
+                            imgDiv.attr("src", imgSrc);
+                            linkDiv.append(imgDiv);
+
+                            $(".videoList").append(newDiv);
+
+
+
+                        });
+
+                }
+
+            });
+
+
+    }
+
+
+    else {
+
+        // WHEN A USER SEARCHES FOR A PLAYLIST
+        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=phish&type=playlist&key=" + APIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+            .then(function (response) {
+                // console.log(response);
+                for (var i = 0; i < 5; i++) {
+                    var firstVideo = "";
+                    var playlistId = response.items[i].id.playlistId;
+                    // console.log(playlistId)
+                    var queryURL2 = "https://www.googleapis.com/youtube/v3/playlists?key=" + APIKey + "&part=snippet&id=" + playlistId;
+
+                    $.ajax({
+                        url: queryURL2,
+                        method: "GET"
+                    })
+
+                        .then(function (response) {
+
+                            console.log(response)
+                            var newDiv = $("<div>");
+                            var titleDiv = $("<h5>");
+                            var linkDiv = $("<a>");
+                            var videoLink = "https://www.youtube.com/watch?v=" + playlistId;
+                            var imgDiv = $("<img>");
+                            // console.log(response)
+                            // console.log(response)
+                            var imgSrc = response.items[0].snippet.thumbnails.medium.url;
+
+                            titleDiv.text(response.items[0].snippet.title);
+                            newDiv.append(titleDiv);
+
+
+
+                            linkDiv.attr("href", videoLink);
+                            linkDiv.attr("target", "_blank");
+                            newDiv.append(linkDiv);
+
+                            imgDiv.attr("src", imgSrc);
+                            linkDiv.append(imgDiv);
+
+                            $("#yt-content").append(newDiv);
+
+                            // console.log(videoLink);
+
+                        });
+
+                }
+
+            });
+    };
 });
