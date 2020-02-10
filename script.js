61ce8da24344388a29e8f3dfa6542b0c8aed9bbf
ctsalva <coreysalva@gmail.com>
2020-02-10 18:18:56 -0500

    pushing Ryan's script file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Copy to clipboard function
 function copyToClipboard(text) {
     var dummy = document.createElement("textarea");
     // to avoid breaking orgain page when copying more words
@@ -14,18 +15,24 @@ function copyToClipboard(text) {
 
 var APIKey = "AIzaSyAU1fAk-8WcXwrIryta0wlz5iYnRE_c2ZA";
 
-
+// Event listener for search button
 $("#search-btn").on("click", function () {
     $("#yt-content").empty();
+    $("#tk-content").empty();
     var songName = $("#song_name").val().trim();
     var artistName = $("#artist_name").val().trim();
     var albumName = $("#album_name").val().trim();
 
+    // Combine all search terms for youtube search
     var searchTerm = songName + "+" + artistName + "+" + albumName;
+
     // replace blanks with "+"s
     searchTerm = searchTerm.replace(/ /g, "+");
+    artistName = artistName.replace(/ /g, "+");
+
     // replace ++ with +
     searchTerm = searchTerm.replace("++", "+");
+
     // remove + from beginning of search term
     while (searchTerm.charAt(0) === '+') {
         searchTerm = searchTerm.substr(1);
@@ -38,6 +45,7 @@ $("#search-btn").on("click", function () {
     // WHEN USER DOES A VIDEO KEYWORD SEARCH..........
     if ($("select[id=searchType] option:selected").val() == "videos") {
 
+        // query to find video ID based on search term
         var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=" + searchTerm + "&type=video&key=" + APIKey;
         $.ajax({
             url: queryURL,
@@ -46,11 +54,13 @@ $("#search-btn").on("click", function () {
 
             .then(function (response) {
 
+                // creates loop for 5 results
                 for (var i = 0; i < 5; i++) {
 
                     const videoId = response.items[i].id.videoId;
                     const buttonId = ("button" + i);
 
+                    // query to get video details
                     var queryURL2 = "https://www.googleapis.com/youtube/v3/videos?key=" + APIKey + "&part=snippet&id=" + videoId;
 
                     $.ajax({
@@ -60,23 +70,29 @@ $("#search-btn").on("click", function () {
 
                         .then(function (response) {
 
+                            // create new div for title/video
                             var newDiv = $("<div>");
                             var titleDiv = $("<h5>");
-                            var linkDiv = $("<a>");
+                            // var linkDiv = $("<a>");
                             var videoLink = "https://www.youtube.com/watch?v=" + videoId;
-                            var imgDiv = $("<img>");
+                            // var imgDiv = $("<img>");
+                            var embedDiv = '<div class="video-container"><iframe width="853" height="480" src="http://www.youtube.com/embed/' + videoId + '"?rel=0" frameborder="0" allowfullscreen></iframe></div>';
+                            console.log(embedDiv);
                             var buttonCode = '<br><a id="' + buttonId + '" class="waves-effect waves-light btn" data-url="' + videoLink + '" ><i class="fas fa-clipboard" aria-hidden="true"></i> Copy</a>'
 
-                            var imgSrc = response.items[0].snippet.thumbnails.medium.url;
+                            // var imgSrc = response.items[0].snippet.thumbnails.medium.url;
 
                             titleDiv.text(response.items[0].snippet.title);
                             newDiv.append(titleDiv);
-                            linkDiv.attr("href", videoLink);
-                            linkDiv.attr("target", "_blank");
-                            newDiv.append(linkDiv);
-                            imgDiv.attr("src", imgSrc);
-                            linkDiv.append(imgDiv);
+                            // linkDiv.attr("href", videoLink);
+                            // linkDiv.attr("target", "_blank");
+                            // newDiv.append(linkDiv);
+                            // imgDiv.attr("src", imgSrc);
+                            // linkDiv.append(imgDiv);
+                            newDiv.append(embedDiv);
                             newDiv.append(buttonCode);
+
+                            // copy to clipboard
                             $(".btn").click(function () {
 
                                 var dummy = document.createElement("textarea");
@@ -91,6 +107,8 @@ $("#search-btn").on("click", function () {
                                 document.execCommand("copy");
                                 document.body.removeChild(dummy);
                             });
+
+                            // append to existing div
                             $("#yt-content").append(newDiv);
 
                             // console.log($("#button0").data("url"));
@@ -106,7 +124,7 @@ $("#search-btn").on("click", function () {
     // WHEN A USER SEARCHES FOR CHANNEL
     else if ($("select[id=searchType] option:selected").val() == "channels") {
 
-
+        // query for channel ID based on search term
         var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=" + searchTerm + "&type=channel&key=" + APIKey;
         $.ajax({
             url: queryURL,
@@ -117,10 +135,13 @@ $("#search-btn").on("click", function () {
 
             .then(function (response) {
 
+                // creates loop for 5 results
                 for (var i = 0; i < 5; i++) {
 
                     const channelId = response.items[i].id.channelId;
                     const buttonId = ("button" + i);
+
+                    // query to get video details
                     var queryURL2 = "https://www.googleapis.com/youtube/v3/channels?key=" + APIKey + "&part=snippet&id=" + channelId;
 
                     $.ajax({
@@ -130,6 +151,7 @@ $("#search-btn").on("click", function () {
 
                         .then(function (response) {
 
+                            // create new div for title/description/image w/ link
                             var newDiv = $("<div>");
                             var titleDiv = $("<h5>");
                             var descriptionDiv = $("<p>");
@@ -169,6 +191,7 @@ $("#search-btn").on("click", function () {
                                 document.body.removeChild(dummy);
                             });
 
+                            // append to existing div
                             $("#yt-content").append(newDiv);
 
 
@@ -185,7 +208,7 @@ $("#search-btn").on("click", function () {
     // WHEN A USER SEARCHES FOR A PLAYLIST
     else {
 
-
+        // query to find playlist ID based on search term
         var queryURL = "https://www.googleapis.com/youtube/v3/search?part=id&q=" + searchTerm + "&type=playlist&key=" + APIKey;
         $.ajax({
             url: queryURL,
@@ -197,6 +220,8 @@ $("#search-btn").on("click", function () {
 
                     const playlistId = response.items[i].id.playlistId;
                     const buttonId = ("button" + i);
+
+                    // query to get playlist details
                     var queryURL2 = "https://www.googleapis.com/youtube/v3/playlists?key=" + APIKey + "&part=snippet&id=" + playlistId;
 
                     $.ajax({
@@ -206,23 +231,26 @@ $("#search-btn").on("click", function () {
 
                         .then(function (response) {
 
+                            // create new div for title/video
                             var newDiv = $("<div>");
                             var titleDiv = $("<h5>");
-                            var linkDiv = $("<a>");
+                            // var linkDiv = $("<a>");
 
                             var videoLink = "https://www.youtube.com/playlist?list=" + playlistId;
 
-                            var imgDiv = $("<img>");
-                            var imgSrc = response.items[0].snippet.thumbnails.medium.url;
+                            // var imgDiv = $("<img>");
+                            // var imgSrc = response.items[0].snippet.thumbnails.medium.url;
+                            var embedDiv = '<div class="video-container"><iframe width="853" height="480" src="http://www.youtube.com/embed/videoseries?list=' + playlistId + '"?rel=0" frameborder="0" allowfullscreen></iframe></div>';
                             var buttonCode = '<br><a id="' + buttonId + '" class="waves-effect waves-light btn" data-url="' + videoLink + '" ><i class="fas fa-clipboard" aria-hidden="true"></i> Copy</a>'
 
                             titleDiv.text(response.items[0].snippet.title);
                             newDiv.append(titleDiv);
-                            linkDiv.attr("href", videoLink);
-                            linkDiv.attr("target", "_blank");
-                            newDiv.append(linkDiv);
-                            imgDiv.attr("src", imgSrc);
-                            linkDiv.append(imgDiv);
+                            // linkDiv.attr("href", videoLink);
+                            // linkDiv.attr("target", "_blank");
+                            // newDiv.append(linkDiv);
+                            // imgDiv.attr("src", imgSrc);
+                            // linkDiv.append(imgDiv);
+                            newDiv.append(embedDiv);
                             newDiv.append(buttonCode);
                             $(".btn").click(function () {
 
@@ -239,6 +267,7 @@ $("#search-btn").on("click", function () {
                                 document.body.removeChild(dummy);
                             });
 
+                            // append to existing div
                             $("#yt-content").append(newDiv);
 
 
@@ -248,4 +277,76 @@ $("#search-btn").on("click", function () {
 
             });
     };
+
+
+
+
+    //     var lastFmApiKey = "57fffbb92b9278298f1b78c87983014b"
+    //     var sharedSecret = "7a7564505d37a6b2f3572896ce7d994a"
+    //     var queryURL5 = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistName + "&api_key=" + lastFmApiKey + "&format=json";
+    //     $.ajax({
+    //         url: queryURL5,
+    //         method: "GET"
+    //     })
+
+    //         .then(function (response) {
+    //             // console.log(response);
+
+    //             var newDiv = $("<div>");
+    //             var artistBioTitle = $("<h5>");
+    //             var artistBioText = $("<p>");
+    //             var similarDivTitle = $("<h5>");
+    //             var similarDivText = $("<p>");
+    //             var albumDivHeader = $("<h5>");
+
+
+    //             similarDivTitle.text("Similar Artists:")
+    //             similarDivText.text()
+    //             artistBioTitle.text("Artist Biography:");
+    //             artistBioText.text(response.artist.bio.summary)
+    //             newDiv.append(artistBioTitle);
+    //             newDiv.append(artistBioText);
+    //             newDiv.append("<br>");
+    //             albumDivHeader.text("Albums:");
+    //             newDiv.append(albumDivHeader);
+
+    //             $("#tk-content").append(newDiv);
+
+    //         });
+
+
+    //     var consumerKey = "gzWAlJdZuJtEWPZkzhui";
+    //     var consumerSecret = "PMLUKaQuCUoGBmQBXcRLDyqzltgJxUHH";
+    //     var queryURL4 = "https://api.discogs.com/database/search?artist=" + artistName + "&format=album&key=" + consumerKey + "&secret=" + consumerSecret;
+
+
+
+    //     $.ajax({
+    //         url: queryURL4,
+    //         method: "GET"
+    //     })
+
+    //         .then(function (response) {
+    //             for (var i = 0; i < 10; i++) {
+    //                 // console.log(response);
+
+    //                 var newDiv = $("<div>");
+    //                 var albumTitleDiv = $("<p>");
+    //                 var imgDiv = $("<img>");
+    //                 var imgSrc = response.results[i].cover_image;
+
+    //                 imgDiv.attr("src", imgSrc);
+    //                 albumTitleDiv.text(response.results[i].title);
+    //                 newDiv.append(albumTitleDiv);
+    //                 newDiv.append(imgDiv);
+    //                 newDiv.append("<br>");
+    //                 newDiv.append("<br>");
+
+    //                 $("#tk-content").append(newDiv);
+    //             };
+
+    //         });
+
+
+
 });
