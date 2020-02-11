b7e72880a4263f39ec3a28782c6bff44c5a5459f
BDejesus1999 <branden.m.dejesus99@gmail.com>
2020-02-10 23:11:37 -0500

    Items from consle being appended

--- Project 1 example.js
+++ Project 1 example.js
@@ -23,71 +23,33 @@ $("#search-btn").on("click", function () {
                 console.log(json);
                 // Parse the response.
                 // Do other things.
+
+                var newDiv = $('<div>');
+                for (i = 0; i < 5; i++) {
+
+
+                    var titleDiv = $('<h5>');
+                    var linkDiv = $('<a>');
+                    var embedDiv = '<div class="ticket-container"></div>'
+                    console.log(embedDiv);
+                    var eventImage = $('<img>').attr('src', json._embedded.events[i].images[0]);
+
+                    $('#tk-content').append(titleDiv);
+                    $('#tk-content').append(embedDiv);
+                    titleDiv.html(json._embedded.events[i].name);
+                    newDiv.append(titleDiv);
+                    newDiv.append(embedDiv);
+                    newDiv.append();
+                    $("#tk-content").append(newDiv);
+                }
             },
             error: function (xhr, status, err) {
                 // This time, we do not end up here!
             }
+
+
+
         })
-        if ()
-            $("#tk-content").append()
 
     })
-
-
-    // find events (below will)
-    function getEvents(page) {
-
-        $('#events-panel').show();
-
-        if (page < 0) {
-            page = 0;
-            return;
-        }
-        if (page > 0) {
-            if (page > getEvents.json.page.totalPages - 1) {
-                page = 0;
-            }
-        }
-
-        $.ajax({
-            type: "GET",
-            url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apikey + "&size=5&page=" + page,
-            async: true,
-            dataType: "json",
-            success: function (json) {
-                getEvents.json = json;
-                showEvents(json);
-            },
-            error: function (xhr, status, err) {
-                console.log(err);
-            }
-        });
-    }
-    //To show the events
-    function showEvents(json) {
-        var items = $('#events .list-group-item');
-        items.hide();
-        var events = json._embedded.events;
-        var item = items.first();
-        for (var i = 0; i < events.length; i++) {
-            item.children('.list-group-item-heading').text(events[i].name);
-            item.children('.list-group-item-text').text(events[i].dates.start.localDate);
-            try {
-                item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
-            } catch (err) {
-                console.log(err);
-            }
-            item.show();
-            item.off("click");
-            item.click(events[i], function (eventObject) {
-                console.log(eventObject.data);
-                try {
-                    getAttraction(eventObject.data._embedded.attractions[0].id);
-                } catch (err) {
-                    console.log(err);
-                }
-            });
-            item = item.next();
-        }
-    }
 })
