f44e9bf1f593784151bd0131209b19fe90c48861
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-11 11:50:15 -0500

    reverted links back to new tabs

--- script.js
+++ script.js
@@ -73,23 +73,23 @@ $("#search-btn").on("click", function () {
                             // create new div for title/video
                             var newDiv = $("<div>");
                             var titleDiv = $("<h5>");
-                            // var linkDiv = $("<a>");
+                            var linkDiv = $("<a>");
                             var videoLink = "https://www.youtube.com/watch?v=" + videoId;
-                            // var imgDiv = $("<img>");
-                            var embedDiv = '<div class="video-container"><iframe width="853" height="480" src="http://www.youtube.com/embed/' + videoId + '?rel=0" frameborder="0" allowfullscreen></iframe></div>';
-                            console.log(embedDiv);
-                            var buttonCode = '<br><a id="' + buttonId + '" class="waves-effect waves-light btn" data-url="' + videoLink + '" ><i class="fas fa-clipboard" aria-hidden="true"></i> Copy</a>'
+                            var imgDiv = $("<img>");
+                            // var embedDiv = '<div class="video-container"><iframe width="853" height="480" src="http://www.youtube.com/embed/' + videoId + '?rel=0" frameborder="0" allowfullscreen></iframe></div>';
+                            var buttonCode = '<br><a id="' + buttonId + '" class="waves-effect waves-light btn" data-url="' + videoLink + '" ><i class="fas fa-clipboard" aria-hidden="true"></i> Copy</a>';
 
-                            // var imgSrc = response.items[0].snippet.thumbnails.medium.url;
+                            var imgSrc = response.items[0].snippet.thumbnails.medium.url;
+                            console.log(imgSrc);
 
                             titleDiv.text(response.items[0].snippet.title);
                             newDiv.append(titleDiv);
-                            // linkDiv.attr("href", videoLink);
-                            // linkDiv.attr("target", "_blank");
-                            // newDiv.append(linkDiv);
-                            // imgDiv.attr("src", imgSrc);
-                            // linkDiv.append(imgDiv);
-                            newDiv.append(embedDiv);
+                            linkDiv.attr("href", videoLink);
+                            linkDiv.attr("target", "_blank");
+                            newDiv.append(linkDiv);
+                            imgDiv.attr("src", imgSrc);
+                            linkDiv.append(imgDiv);
+                            // newDiv.append(embedDiv);
                             newDiv.append(buttonCode);
 
                             // copy to clipboard
@@ -234,22 +234,22 @@ $("#search-btn").on("click", function () {
                             // create new div for title/video
                             var newDiv = $("<div>");
                             var titleDiv = $("<h5>");
-                            // var linkDiv = $("<a>");
+                            var linkDiv = $("<a>");
 
                             var videoLink = "https://www.youtube.com/playlist?list=" + playlistId;
                             var imgDiv = $("<img>");
                             var imgSrc = response.items[0].snippet.thumbnails.medium.url;
-                            var embedDiv = '<div class="video-container"><iframe width="853" height="480" src="http://www.youtube.com/embed/videoseries?list=' + playlistId + '?rel=0" frameborder="0" allowfullscreen></iframe></div>';
-                            var buttonCode = '<br><a id="' + buttonId + '" class="waves-effect waves-light btn" data-url="' + videoLink + '" ><i class="fas fa-clipboard" aria-hidden="true"></i> Copy</a>'
+                            // var embedDiv = '<div class="video-container"><iframe width="853" height="480" src="http://www.youtube.com/embed/videoseries?list=' + playlistId + '?rel=0" frameborder="0" allowfullscreen></iframe></div>';
+                            var buttonCode = '<br><a id="' + buttonId + '" class="waves-effect waves-light btn" data-url="' + videoLink + '" ><i class="fas fa-clipboard" aria-hidden="true"></i> Copy</a>';
 
                             titleDiv.text(response.items[0].snippet.title);
                             newDiv.append(titleDiv);
-                            // linkDiv.attr("href", videoLink);
-                            // linkDiv.attr("target", "_blank");
-                            // newDiv.append(linkDiv);
-                            // imgDiv.attr("src", imgSrc);
-                            // linkDiv.append(imgDiv);
-                            newDiv.append(embedDiv);
+                            linkDiv.attr("href", videoLink);
+                            linkDiv.attr("target", "_blank");
+                            newDiv.append(linkDiv);
+                            imgDiv.attr("src", imgSrc);
+                            linkDiv.append(imgDiv);
+                            // newDiv.append(embedDiv);
                             newDiv.append(buttonCode);
                             $(".btn").click(function () {
 
